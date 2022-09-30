9e7b62c49404090e339bbb91930a6a878e97a484
Jonathan Papineau <dev@jontech.app>
2022-09-29 21:06:07 -0400

    Initial commit

---
+++ src/boards/main/hal/gcc/gcc/system_same51.d
@@ -0,0 +1,153 @@
+gcc/system_same51.d gcc/system_same51.o: ../gcc/system_same51.c \
+ ../include/same51.h ../include/same51j18a.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ ../CMSIS/Core/Include/core_cm4.h ../CMSIS/Core/Include/cmsis_version.h \
+ ../CMSIS/Core/Include/cmsis_compiler.h ../CMSIS/Core/Include/cmsis_gcc.h \
+ ../CMSIS/Core/Include/mpu_armv7.h ../include/system_same51.h \
+ ../include/component/ac.h ../include/component/adc.h \
+ ../include/component/aes.h ../include/component/can.h \
+ ../include/component/ccl.h ../include/component/cmcc.h \
+ ../include/component/dac.h ../include/component/dmac.h \
+ ../include/component/dsu.h ../include/component/eic.h \
+ ../include/component/evsys.h ../include/component/freqm.h \
+ ../include/component/gclk.h ../include/component/hmatrixb.h \
+ ../include/component/icm.h ../include/component/i2s.h \
+ ../include/component/mclk.h ../include/component/nvmctrl.h \
+ ../include/component/oscctrl.h ../include/component/osc32kctrl.h \
+ ../include/component/pac.h ../include/component/pcc.h \
+ ../include/component/pdec.h ../include/component/pm.h \
+ ../include/component/port.h ../include/component/qspi.h \
+ ../include/component/ramecc.h ../include/component/rstc.h \
+ ../include/component/rtc.h ../include/component/sdhc.h \
+ ../include/component/sercom.h ../include/component/supc.h \
+ ../include/component/tc.h ../include/component/tcc.h \
+ ../include/component/trng.h ../include/component/usb.h \
+ ../include/component/wdt.h ../include/instance/ac.h \
+ ../include/instance/adc0.h ../include/instance/adc1.h \
+ ../include/instance/aes.h ../include/instance/can0.h \
+ ../include/instance/can1.h ../include/instance/ccl.h \
+ ../include/instance/cmcc.h ../include/instance/dac.h \
+ ../include/instance/dmac.h ../include/instance/dsu.h \
+ ../include/instance/eic.h ../include/instance/evsys.h \
+ ../include/instance/freqm.h ../include/instance/gclk.h \
+ ../include/instance/hmatrix.h ../include/instance/icm.h \
+ ../include/instance/i2s.h ../include/instance/mclk.h \
+ ../include/instance/nvmctrl.h ../include/instance/oscctrl.h \
+ ../include/instance/osc32kctrl.h ../include/instance/pac.h \
+ ../include/instance/pcc.h ../include/instance/pdec.h \
+ ../include/instance/pm.h ../include/instance/port.h \
+ ../include/instance/pukcc.h ../include/instance/qspi.h \
+ ../include/instance/ramecc.h ../include/instance/rstc.h \
+ ../include/instance/rtc.h ../include/instance/sdhc0.h \
+ ../include/instance/sercom0.h ../include/instance/sercom1.h \
+ ../include/instance/sercom2.h ../include/instance/sercom3.h \
+ ../include/instance/sercom4.h ../include/instance/sercom5.h \
+ ../include/instance/supc.h ../include/instance/tc0.h \
+ ../include/instance/tc1.h ../include/instance/tc2.h \
+ ../include/instance/tc3.h ../include/instance/tc4.h \
+ ../include/instance/tc5.h ../include/instance/tcc0.h \
+ ../include/instance/tcc1.h ../include/instance/tcc2.h \
+ ../include/instance/tcc3.h ../include/instance/tcc4.h \
+ ../include/instance/trng.h ../include/instance/usb.h \
+ ../include/instance/wdt.h ../include/pio/same51j18a.h
+../include/same51.h:
+../include/same51j18a.h:
+/usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h:
+../CMSIS/Core/Include/core_cm4.h:
+../CMSIS/Core/Include/cmsis_version.h:
+../CMSIS/Core/Include/cmsis_compiler.h:
+../CMSIS/Core/Include/cmsis_gcc.h:
+../CMSIS/Core/Include/mpu_armv7.h:
+../include/system_same51.h:
+../include/component/ac.h:
+../include/component/adc.h:
+../include/component/aes.h:
+../include/component/can.h:
+../include/component/ccl.h:
+../include/component/cmcc.h:
+../include/component/dac.h:
+../include/component/dmac.h:
+../include/component/dsu.h:
+../include/component/eic.h:
+../include/component/evsys.h:
+../include/component/freqm.h:
+../include/component/gclk.h:
+../include/component/hmatrixb.h:
+../include/component/icm.h:
+../include/component/i2s.h:
+../include/component/mclk.h:
+../include/component/nvmctrl.h:
+../include/component/oscctrl.h:
+../include/component/osc32kctrl.h:
+../include/component/pac.h:
+../include/component/pcc.h:
+../include/component/pdec.h:
+../include/component/pm.h:
+../include/component/port.h:
+../include/component/qspi.h:
+../include/component/ramecc.h:
+../include/component/rstc.h:
+../include/component/rtc.h:
+../include/component/sdhc.h:
+../include/component/sercom.h:
+../include/component/supc.h:
+../include/component/tc.h:
+../include/component/tcc.h:
+../include/component/trng.h:
+../include/component/usb.h:
+../include/component/wdt.h:
+../include/instance/ac.h:
+../include/instance/adc0.h:
+../include/instance/adc1.h:
+../include/instance/aes.h:
+../include/instance/can0.h:
+../include/instance/can1.h:
+../include/instance/ccl.h:
+../include/instance/cmcc.h:
+../include/instance/dac.h:
+../include/instance/dmac.h:
+../include/instance/dsu.h:
+../include/instance/eic.h:
+../include/instance/evsys.h:
+../include/instance/freqm.h:
+../include/instance/gclk.h:
+../include/instance/hmatrix.h:
+../include/instance/icm.h:
+../include/instance/i2s.h:
+../include/instance/mclk.h:
+../include/instance/nvmctrl.h:
+../include/instance/oscctrl.h:
+../include/instance/osc32kctrl.h:
+../include/instance/pac.h:
+../include/instance/pcc.h:
+../include/instance/pdec.h:
+../include/instance/pm.h:
+../include/instance/port.h:
+../include/instance/pukcc.h:
+../include/instance/qspi.h:
+../include/instance/ramecc.h:
+../include/instance/rstc.h:
+../include/instance/rtc.h:
+../include/instance/sdhc0.h:
+../include/instance/sercom0.h:
+../include/instance/sercom1.h:
+../include/instance/sercom2.h:
+../include/instance/sercom3.h:
+../include/instance/sercom4.h:
+../include/instance/sercom5.h:
+../include/instance/supc.h:
+../include/instance/tc0.h:
+../include/instance/tc1.h:
+../include/instance/tc2.h:
+../include/instance/tc3.h:
+../include/instance/tc4.h:
+../include/instance/tc5.h:
+../include/instance/tcc0.h:
+../include/instance/tcc1.h:
+../include/instance/tcc2.h:
+../include/instance/tcc3.h:
+../include/instance/tcc4.h:
+../include/instance/trng.h:
+../include/instance/usb.h:
+../include/instance/wdt.h:
+../include/pio/same51j18a.h:
---
+++ src/boards/main/main.cpp
@@ -0,0 +1,12 @@
+#include <atmel_start.h>
+
+
+int main()
+{
+	/* Initializes MCU, drivers and middleware */
+	atmel_start_init();
+
+	/* Replace with your application code */
+	while (1) {
+	}
+}
