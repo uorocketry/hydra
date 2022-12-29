659e1713a87c84e401ad7c66bf69de0587955fc2
Jonathan Papineau <dev@jontech.app>
2022-12-29 11:54:48 -0500

    refactor: cleanup

--- libraries/common-arm/src/lib.rs
+++ libraries/common-arm/src/lib.rs
@@ -6,8 +6,12 @@
 //!
 
 mod error;
-pub mod sd;
+mod sd;
 
 pub use crate::error::error_manager::ErrorManager;
 pub use crate::error::hydra_error::SpawnError;
 pub use crate::sd::SdInterface;
+
+use defmt_rtt as _; // global logger
+
+use panic_halt as _;
