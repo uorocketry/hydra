0aa1368335cc7a8408e50cb26402cf90b3f35380
Noah Sprenger <noah.r.sprenger@gmail.com>
2024-06-08 12:34:54 -0400

    Fix: Import

--- boards/recovery/src/state_machine/states/wait_for_recovery.rs
+++ boards/recovery/src/state_machine/states/wait_for_recovery.rs
@@ -3,7 +3,7 @@ use crate::app::monotonics;
 use crate::no_transition;
 use crate::state_machine::{RocketStates, State, StateMachineContext, TransitionInto};
 use crate::types::COM_ID;
-use atsamd_hal::prelude::_atsamd_hal_timer_traits_InterruptDrivenTimer;
+use atsamd_hal::timer_traits::InterruptDrivenTimer;
 use defmt::{write, Format, Formatter};
 use messages::command::{Command, PowerDown, RadioRate, RadioRateChange};
 use messages::sender::Sender::SensorBoard;
